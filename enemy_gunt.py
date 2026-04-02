60af556cb57ec3da5720dadb771a5e1cecefa682
Forge-guy <carson.floyd@students.fhu.edu>
2026-04-02 12:50:58 -0500

    test

--- enemy_gunt.py
+++ enemy_gunt.py
@@ -4,7 +4,7 @@ import math
 from ray import Ray
 class Gunt():
     def __init__(self):
-        self.attack_dist = Ray(3,6)
+        self.attack_dist = Ray(3,6,7)
         self.speed = 0.33
         self.size = 10
         self.health = 100
