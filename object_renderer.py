54a78d41fc6f57a178dee5cfa9b2ffb3ef086ea9
Brady <brady.miller@students.fhu.edu>
2026-04-03 13:48:38 -0500

    Added resources folder, object_renderer.py file, and added texture_size lines  in settings.py file.

---
+++ object_renderer.py
@@ -0,0 +1,15 @@
+import pygame as pg
+from settings import *
+
+class ObjectRenderer:
+    def __init__(self, game):
+        self.game = game
+        self.screen = game.screen
+
+    @staticmethod
+    def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
+        texture = pg.image.load(path).convert_alpha()
+        return pg.transform.scale(texture, res)
+    
+    def load_wall_textures(self):
+        pass
