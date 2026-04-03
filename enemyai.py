f103aa74dc4dfb4cf9762e0c6b715b43928b9754
Jackson Morris <jackson.morris@students.fhu.edu>
2026-04-02 22:42:19 -0500

    Initial setup, raycasting, 3D projection

---
+++ enemyai.py
@@ -0,0 +1,8 @@
+#for when we add sprites
+#from sprite_object import *
+#from random import randint, random, choice
+
+'''class NPC():
+    def __init__ (self, game, path='resources/sprites/npc/soldier/0.png', pos(10.5, 5.5), 
+                 scale=0.6, shift=0.38, animation_time=180):
+        pass'''
