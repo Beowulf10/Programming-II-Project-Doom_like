f53a9d279a4d1c4ede7a2c68c8b753bc517921f6
Brady <brady.miller@students.fhu.edu>
2026-04-03 23:18:55 -0500

    Finished creating an operational sprite function with sprite_object.py and object_handler.py. I should've commited before hand, but I also finished an operational weapon.py and laid the foundation for the sound effects in sound.py. We will need to flesh out weapons and sounds when we can.

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from map import *
 from player import *
 from raycasting import *
 from object_renderer import *
+from sprite_object import *
+from object_handler import *
+from weapon import *
+#from sound import *
 
 
 class Game:
@@ -21,10 +25,20 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
+        self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
+    #   self.sound = Sound(self)
+
+    #    self.static_sprite = SpriteObject(self)
+    #    self.animated_sprite = AnimatedSprite(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
+        self.object_handler.update()
+        self.weapon.update()
+    #    self.static_sprite.update()
+    #    self.animated_sprite.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
@@ -32,6 +46,7 @@ class Game:
     def draw(self):
         #self.screen.fill('black')
         self.object_renderer.draw()
+        self.weapon.draw()
         #self.map.draw()
         #self.player.draw()
 
@@ -40,6 +55,7 @@ class Game:
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            self.player.single_fire_event(event)
 
     def run(self):
         while True:
