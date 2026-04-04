d89e698694fd7bd4bf85463eee7935f3bce2cf66
Brady <brady.miller@students.fhu.edu>
2026-04-03 20:13:05 -0500

    Added mouse controls, wall textures, background textures, and player size

--- object_renderer.py
+++ object_renderer.py
@@ -5,6 +5,25 @@ class ObjectRenderer:
     def __init__(self, game):
         self.game = game
         self.screen = game.screen
+        self.wall_textures = self.load_wall_textures()
+        self.sky_image = self.get_texture('resources/textures/SKY1.png', (WIDTH, HALF_HEIGHT))
+        self.sky_offset = 0
+
+    def draw(self):
+        self.draw_background()
+        self.render_game_objects()
+
+    def draw_background(self):
+        self.sky_offset = (self.sky_offset + 4.5 * self.game.player.rel) % WIDTH
+        self.screen.blit(self.sky_image, (-self.sky_offset, 0))
+        self.screen.blit(self.sky_image, (-self.sky_offset + WIDTH, 0))
+        #floor
+        pg.draw.rect(self.screen, FLOOR_COLOR, (0, HALF_HEIGHT, WIDTH, HEIGHT))
+
+    def render_game_objects(self):
+        list_objects = self.game.raycasting.objects_to_render
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)
 
     @staticmethod
     def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
@@ -12,4 +31,10 @@ class ObjectRenderer:
         return pg.transform.scale(texture, res)
     
     def load_wall_textures(self):
-        pass
+        return {
+            1: self.get_texture('resources/textures/t9.png'),
+            2: self.get_texture('resources/textures/t15.png'),
+            3: self.get_texture('resources/textures/t16.png'),
+            4: self.get_texture('resources/textures/t35.png'),
+            5: self.get_texture('resources/textures/t55.png')
+        }
